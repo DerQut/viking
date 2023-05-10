4ab2df07a28529f75f8c4c5d54a3c4ec3d6ff821
babel <cholodecki@gmail.com>
2023-05-10 19:02:36 +0200

    removed trash

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ import webbrowser
 #rows, cols = (480, 480)
 #all_points = [[0 for i in range(cols)] for j in range(rows)]
 
+pygame.font.init()
+
+norse_font_30 = pygame.font.SysFont("Norse", 30)
+
 class Button:
     all = []
 
-    def __init__(self, id: int, text: str, font_size: int, x_cord: int, y_cord: int, loads_from: str, is_visible=False):
+    def __init__(self, id: int, text: str, font, x_cord: int, y_cord: int, loads_from: str, is_visible=False):
 
         # assert
         assert id > 0, f"ID {id} is lower than 1!"
-        assert font_size > 0, f"Font size {font_size} is lower than 1!"
         assert len(text) > 0, f"Text can't be empty!"
 
         # assign mandatory
         self.id = id
         self.text = text
-        self.font_size = font_size
+        self.font = font
         self.x_cord = x_cord
         self.y_cord = y_cord
         self.loads_from = loads_from
@@ -31,9 +34,26 @@ class Button:
         # assign optional
         self.is_visible = is_visible
 
+        # pygame magic
+        self.pygame_object = self.font.render(self.text, False, (0, 0, 0))
+
+        self.x_size = self.pygame_object.get_width()
+        self.y_size = self.pygame_object.get_height()
+
+
         Button.all.append(self)
+
+    def __repr__(self):
+        return f"Button({self.id}, '{self.text}', {self.font}, {self.x_cord}, {self.y_cord}, '{self.loads_from}', {self.is_visible}, {self.x_size}, {self.y_size}"
+
 
 class Menu_section:
     all = []
 
-    def __init__(self):
+
+button_new_game = Button(1, "New game", norse_font_30, 0, 0, "right", True)
+
+
+if __name__ == "__main__":
+    for button in Button.all:
+        print(button)
